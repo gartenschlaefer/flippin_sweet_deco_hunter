598f65cca35f68ab3d052dac1659d9bf33d4fd99
Eworhard <ete@lumpi-berndorf.at>
2025-12-14 11:14:43 +0100

    Whip length fix

--- scripts/licorice_whip.gd
+++ scripts/licorice_whip.gd
@@ -12,15 +12,22 @@ extends Node3D
 var whip_instance: Node3D
 var segs: Array[RigidBody3D] = []
 var rest_lengths: Array[float] = []
+var rest_dirs_local: Array[Vector3] = []
+
 
 var offsets := []
 
 func _ready():
+	segs.clear()
 	for c in physics.get_children():
 		if c is RigidBody3D:
-			segs.append(c)
+			segs.append(c as RigidBody3D)
 
 	segs.sort_custom(func(a, b): return a.global_position.y < b.global_position.y)
+
+	if segs.size() == 0:
+		push_error("No physics segments found under Physics")
+		return
 
 	offsets.resize(segs.size())
 
@@ -34,14 +41,15 @@ func _ready():
 
 	for i in range(1, segs.size()):
 		_create_joint(segs[i], segs[i - 1])
-		var rest_lengths: Array[float] = []
-	_cache_rest_lengths()
+
+	_cache_rest_data()
 
 
 func _physics_process(delta):
+	stabilize_chain_angles()
 
 	skeleton.clear_bones_global_pose_override()
-	var skel_inv := skeleton.global_transform.affine_inverse()
+	var skel_inv: Transform3D = skeleton.global_transform.affine_inverse()
 
 	for i in segs.size():
 		var bone := i + 1
@@ -49,10 +57,16 @@ func _physics_process(delta):
 		var world_t: Transform3D = seg_pose * offsets[i]
 		var bone_t: Transform3D = skel_inv * world_t
 		skeleton.set_bone_global_pose_override(bone, bone_t, 1.0, true)
-	stabilize_chain()
 
 
-func stabilize_chain():
+func stabilize_chain_angles():
+	if rest_lengths.size() != segs.size() - 1:
+		return
+	if rest_dirs_local.size() != segs.size() - 1:
+		return
+
+	var max_x := deg_to_rad(physics_allow_segment_x)
+
 	for i in range(1, segs.size()):
 		var a := segs[i - 1]
 		var b := segs[i]
@@ -62,28 +76,60 @@ func stabilize_chain():
 		if len == 0.0:
 			continue
 
+		var n := dir / len
 		var target := rest_lengths[i - 1]
-		var error := len - target
 
-		if abs(error) < 0.0001:
+		var forward: Vector3 = a.global_transform.basis * rest_dirs_local[i - 1]
+		forward = forward.normalized()
+
+		var dotv : float = clamp(forward.dot(n), -1.0, 1.0)
+		var angle := acos(dotv)
+
+		if angle <= max_x:
+			if abs(len - target) > 0.0001:
+				b.global_position = a.global_position + n * target
 			continue
 
-		var n := dir / len
-		var correction := -n * error * chain_stiffness
+		var axis := forward.cross(n)
+		var axis_len := axis.length()
+		if axis_len == 0.0:
+			b.global_position = a.global_position + forward * target
+			continue
 
-		b.global_position += correction
+		axis /= axis_len
+
+		var excess := angle - max_x
+		var corr: float = excess * clamp(chain_stiffness, 0.0, 1.0)
+
+		var q := Quaternion(axis, -corr)
+		var new_dir := (q * n).normalized()
+
+		b.global_position = a.global_position + new_dir * target
 
 		var v := b.linear_velocity
-		var radial := n * v.dot(n)
+		var radial := new_dir * v.dot(new_dir)
 		b.linear_velocity = v - radial
 
 
-func _cache_rest_lengths():
+func _cache_rest_data():
 	rest_lengths.clear()
+	rest_dirs_local.clear()
+
 	for i in range(1, segs.size()):
-		rest_lengths.append(
-			segs[i].global_position.distance_to(segs[i - 1].global_position)
-		)
+		var a := segs[i - 1]
+		var b := segs[i]
+
+		var d := b.global_position - a.global_position
+		var len := d.length()
+		rest_lengths.append(len)
+
+		var n := Vector3.ZERO
+		if len > 0.0:
+			n = d / len
+
+		var a_inv: Basis = a.global_transform.basis.inverse()
+		rest_dirs_local.append(a_inv * n)
+
 
 
 func _create_joint(a: RigidBody3D, b: RigidBody3D) -> Generic6DOFJoint3D:
