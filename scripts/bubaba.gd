b8767ecb6c2eca76e12bdfa81c5d8cab08a9b033
Gartenschläfer <christian.walter@mailfence.com>
2025-12-15 11:55:57 +0100

    bubaba can get damage

--- scripts/bubaba.gd
+++ scripts/bubaba.gd
@@ -4,12 +4,14 @@
 class_name Bubaba extends CharacterBody3D
 
 # refs
+@export var health_bar: ProgressBar
 @onready var nav_agent = $NavigationAgent3D
 @onready var anim: AnimatedSprite3D = $anim
 
 # vars
 var player_in_reach = false
 var hit_cooldown_timer: Timer = null
+var is_ko = false
 
 # const
 const speed = 2.0
@@ -17,21 +19,96 @@ const evil_to_normal_distance = 3.0
 const anim_normal_idle = &"normal_idle"
 const anim_evil_idle = &"evil_idle"
 const hit_cooldown_time = 2.0
+const whip_damage_to_be_taken = 50.0
 
 
 func _ready():
 
-	# settup
+	# setup
 	player_in_reach = false
+	is_ko = false
+
+	# set init value
+	health_bar.set_value(100)
 
 
 func _physics_process(_delta):
 	
+	# k.o.
+	if is_ko: 
+		move_and_slide()
+		return
+
 	# to new location
 	nav_agent.set_velocity((nav_agent.get_next_path_position() - self.global_transform.origin).normalized() * speed)
 
 	# player interaction
-	_player_interaction()
+	self._player_interaction()
+
+	# weapon interaction
+	self._weapon_interaction()
+
+
+func take_damage(collision: KinematicCollision3D):
+
+	# got hit -> hit cooldown timer
+	if not hit_cooldown_timer == null: return
+
+	# create timer
+	hit_cooldown_timer = Timer.new()
+	add_child(hit_cooldown_timer)
+	hit_cooldown_timer.timeout.connect(self._on_hit_cooldown_timer_timeout)
+	hit_cooldown_timer.wait_time = hit_cooldown_time
+	hit_cooldown_timer.start()
+
+	# hit vector + velocity
+	var hit_vector = (self.global_transform.origin - collision.get_position())
+	hit_vector.y = 0.0
+	velocity = velocity + hit_vector.normalized() * 20.0
+
+	# decrease health
+	health_bar.set_value(health_bar.get_value() - whip_damage_to_be_taken)
+
+	# not dead yet
+	if health_bar.get_value(): return
+	self.bubaba_set_to_ko()
+
+
+func bubaba_set_to_ko():
+
+	# k.o. mode
+	is_ko = true
+
+	# end velocity
+	nav_agent.set_velocity(Vector3.ZERO)
+
+	# todo:
+	# set ko anim
+
+
+func update_target_position(target_position):
+
+	# target position
+	nav_agent.set_target_position(target_position)
+
+
+# --
+# private functions
+
+func _player_interaction():
+
+	# player in reach case -> is evil
+	if not player_in_reach: return
+
+	# skip
+	if nav_agent.distance_to_target() < evil_to_normal_distance: return
+
+	# to normal
+	anim.set_animation(anim_normal_idle)
+	player_in_reach = false
+
+
+func _weapon_interaction():
 
 	# weapon interactions
 	for i in range(get_slide_collision_count()):
@@ -47,53 +124,12 @@ func _physics_process(_delta):
 		self.take_damage(collision)
 
 
-func take_damage(collision: KinematicCollision3D):
-
-	# got hit -> hit cooldown timer
-	if not hit_cooldown_timer == null: return
-
-	# create timer
-	hit_cooldown_timer = Timer.new()
-	add_child(hit_cooldown_timer)
-	hit_cooldown_timer.timeout.connect(self._on_hit_cooldown_timer_timeout)
-	hit_cooldown_timer.wait_time = hit_cooldown_time
-	hit_cooldown_timer.start()
-
-	#var hit_vector = (collision.get_position() - self.global_transform.origin)
-	var hit_vector = (self.global_transform.origin - collision.get_position())
-	hit_vector.y = 0.0
-	hit_vector = hit_vector.normalized()
-	#nav_agent.set_velocity(hit_vector * 50.0)
-	#velocity = velocity + hit_vector * 10.0
-	velocity = velocity + hit_vector * 20.0
-
-
-func _player_interaction():
-
-	# player in reach case -> is evil
-	if not player_in_reach: return
-
-	# skip
-	if nav_agent.distance_to_target() < evil_to_normal_distance: return
-
-	# to normal
-	anim.set_animation(anim_normal_idle)
-	player_in_reach = false
-
-
-func update_target_position(target_position):
-
-	# target position
-	nav_agent.set_target_position(target_position)
-
-
 func _on_navigation_agent_3d_target_reached() -> void:
 
 	# skip
 	if player_in_reach: return
 	player_in_reach = true
 	anim.set_animation(anim_evil_idle)
-	print("player in reach!!!")
 
 
 func _on_navigation_agent_3d_velocity_computed(safe_velocity: Vector3) -> void:
